a9af50304f0099d8d62025e0647cbea6029d8e21
Christopher Ried <chris@bargainhunt.us>
2017-09-26 17:57:51 -0500

    First commit

---
+++ R/ggTableTests.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(xtable)
+
+ggTable <- structure(list(), class = "ggTableObject")
+class(ggTable)
+
+# Create dummy data
+salesData <- tibble(District=c(1,1,1,2,2,2),
+                        Name=c("S1","S2","S3","S4","S5","S6"),
+                        NetSales=c(1000,1500,3000,2000,1500,3000),
+                        Units=c(50,120,52,12,64,52)) %>%
+  mutate(AUR = NetSales / Units)
+
+
+ggTable$data <- salesData
+
+
+# Generates District Totals
+districtTotals <- salesData %>%
+  group_by(District) %>%
+  summarize(Name = "Total",
+            NetSales = sum(NetSales),
+            Units = sum(Units),
+            AUR = sum(NetSales) / sum(Units) )
+
+ggTable$groupTotals <- districtTotals
+
+
+
+# Generates Grand Total
+grandTotals <- districtTotals %>%
+  summarize(District = "",
+    Name = "GrandTotal",
+    NetSales = sum(NetSales),
+    Units = sum(Units),
+    AUR = sum(NetSales)/sum(Units),2)
+
+ggTable$grandTotals <- grandTotals
+
+
+ggTable
+
+# Binds district totals to frame
+totals <- rbind(salesData, salesTotals) %>%
+  arrange(District, Name)
+
+#Binds grand total to frame
+totals <- rbind(totals, grandTotals)
+
+#Generate LaTeX table to display
+xtable(totals)
---
+++ R/ggpTable.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+
+#' Creates the ggtable object
+#'
+#'
+#' @return Returns the structure and the template by which the
+#' @examples
+#'
+ggTable <- function()
+{
+  structure(list(), class = "ggTableObject")
+}
